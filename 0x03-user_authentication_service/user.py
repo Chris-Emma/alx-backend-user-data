ea897ed567d69005dfa85fcc24285c62cb2f3674
Chris-Emma <chrisemmaofficial@gmail.com>
2024-09-25 15:35:27 +0000

    user-authentication-service

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+""" User module for database named users.
+Using the mapping declaration pattern.
+Contain the following attributes:
+  -  id
+  - email
+  - hashed_password
+  - session_id
+  - reset_token
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ User class to interact with the MySQL users table
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
